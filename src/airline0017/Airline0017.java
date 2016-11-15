d6aaf7b48a50aca45f06932a5458875c9cc39241
gunnar <gunnar@localhost>
2016-11-15 15:09:20 +0100

    first empty

---
+++ src/airline0017/Airline0017.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline0017;
+
+/**
+ *
+ * @author gunnar
+ */
+public class Airline0017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
